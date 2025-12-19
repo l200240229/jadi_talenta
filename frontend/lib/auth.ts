e844fba319a16484f6019c20b3967a88ecae36ac
l200240229 <l200240229@student.ums.ac.id>
2025-12-19 11:28:57 +0700

    menambahkan helper untuk token

--- frontend/lib/auth.ts
+++ frontend/lib/auth.ts
@@ -3,3 +3,13 @@ export function logout() {
   localStorage.removeItem("refresh_token");
   window.location.href = "/login";
 }
+
+export const getAccessToken = () =>
+  typeof window !== "undefined"
+    ? localStorage.getItem("access_token")
+    : null;
+
+export const setTokens = (access: string, refresh: string) => {
+  localStorage.setItem("access_token", access);
+  localStorage.setItem("refresh_token", refresh);
+}
